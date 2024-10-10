4f5bafbf11072409193037e67074265bcde133a8
SHIVA RAGHAV <139120737+RaghavShiva@users.noreply.github.com>
2024-10-10 19:21:08 +0530

    Time: 25 ms (51.18%) | Memory: 31.4 MB (71.62%) - LeetSync

--- 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
+++ 966-binary-subarrays-with-sum/binary-subarrays-with-sum.cpp
@@ -1,23 +1,48 @@
 class Solution {
 public:
-    int numSubarraysWithSum(vector<int>& nums, int goal) {
+    int count_k(vector<int>&nums, int k){
+        int cnt = 0;
         int n = nums.size();
-        int ans = 0;
-        for(int i = 1; i < n; i++){
-            nums[i] += nums[i - 1];
-        }
+        int o = 0, j = 0;
         for(int i = 0; i < n; i++){
-            int t;
-            if(i == 0){
-                t = 0;
+            while(o < k && j < n){
+                if(nums[j] == 1){
+                    o++;
+                }
+                j++;
             }
-            else{
-                t = nums[i-1];
+            if(o >= k){
+                cnt += n - max(i,j-1);
             }
-            int low_lim = lower_bound(nums.begin(), nums.end(), t + goal) - nums.begin();
-            int upp_lim = upper_bound(nums.begin(), nums.end(), t + goal) - nums.begin();
-            ans += upp_lim - max(low_lim,i);
+            if(nums[i] == 1){
+                o--;
+            }
         }
-        return ans;
+        return cnt;
+    }
+    int numSubarraysWithSum(vector<int>& nums, int goal) {
+        
+        return count_k(nums, goal) - count_k(nums, goal + 1) ; 
+
+
+
+        // int n = nums.size();
+        // int ans = 0;
+        // for(int i = 1; i < n; i++){
+        //     nums[i] += nums[i - 1];
+        // }
+        // for(int i = 0; i < n; i++){
+        //     int t;
+        //     if(i == 0){
+        //         t = 0;
+        //     }
+        //     else{
+        //         t = nums[i-1];
+        //     }
+        //     int low_lim = lower_bound(nums.begin(), nums.end(), t + goal) - nums.begin();
+        //     int upp_lim = upper_bound(nums.begin(), nums.end(), t + goal) - nums.begin();
+        //     ans += upp_lim - max(low_lim,i);
+        // }
+        // return ans;
     }
 };
