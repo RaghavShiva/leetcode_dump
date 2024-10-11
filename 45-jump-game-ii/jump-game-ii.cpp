87ffd769f08b6c82f854e69f337151431cae8382
SHIVA RAGHAV <139120737+RaghavShiva@users.noreply.github.com>
2024-10-11 14:07:17 +0530

    Time: 3 ms (99.06%) | Memory: 19.3 MB (78.94%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -7,7 +7,7 @@ public:
         if(n == 1){
             return 0;
         }
-        while(r < n){
+        while(r < n - 1){
             int mx = 0;
             for(int i = l; i <= r; i++){
                 mx = max(mx, i + nums[i]);
@@ -15,9 +15,6 @@ public:
             ans++;
             l = r + 1;
             r = mx;
-            if(r >= n - 1){
-                return ans;
-            }
         }
         return ans;
     }
