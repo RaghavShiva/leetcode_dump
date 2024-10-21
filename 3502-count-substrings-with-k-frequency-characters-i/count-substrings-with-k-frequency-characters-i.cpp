603e98e07ac58cd89a977ebd2b45bf780bec01ef
SHIVA RAGHAV <139120737+RaghavShiva@users.noreply.github.com>
2024-10-21 20:51:09 +0530

    Time: 73 ms (50.00%) | Memory: 17.7 MB (50.00%) - LeetSync

--- 3502-count-substrings-with-k-frequency-characters-i/count-substrings-with-k-frequency-characters-i.cpp
+++ 3502-count-substrings-with-k-frequency-characters-i/count-substrings-with-k-frequency-characters-i.cpp
@@ -4,15 +4,12 @@ public:
         int ans = 0;
         int n = s.size();
         for(int i = 0; i < n; i++){
-            bool f = false;
             vector<int> cnt(26,0);
             for(int j = i; j < n; j++){
                 cnt[s[j]-'a']++;
                 if(cnt[s[j]-'a']==k){
-                    f = true;
-                }
-                if(f){
-                    ans++;
+                    ans += n-j;
+                    break;
                 }
             }
         }
