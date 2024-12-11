be016513a06420dcd4dd8ddb07d89bcfbdc515a1
SHIVA RAGHAV <139120737+RaghavShiva@users.noreply.github.com>
2024-12-11 21:20:35 +0530

    Time: 83 ms (46.22%) | Memory: 105 MB (67.70%) - LeetSync

--- 2891-maximum-beauty-of-an-array-after-applying-operation/maximum-beauty-of-an-array-after-applying-operation.cpp
+++ 2891-maximum-beauty-of-an-array-after-applying-operation/maximum-beauty-of-an-array-after-applying-operation.cpp
@@ -5,7 +5,7 @@ public:
         int n = nums.size();
         int ans = 0;
         for(int i = 0; i < n; i++){
-            // int it = lower_bound(nums.begin(),nums.end(),nums[i]-2*k)-nums.begin();
+           
             int it = upper_bound(nums.begin(),nums.end(),nums[i]+2*k)-nums.begin() -i;
             ans = max(ans, it);
         }
