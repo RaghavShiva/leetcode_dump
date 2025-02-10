c9d3311e12b2e965c3a0b8d08894f7ba3185be91
SHIVA RAGHAV <139120737+RaghavShiva@users.noreply.github.com>
2025-02-10 18:52:43 +0530

    Time: 718 ms (42.10%) | Memory: 283.6 MB (31.58%) - LeetSync

--- 3760-assign-elements-to-groups-with-constraints/assign-elements-to-groups-with-constraints.cpp
+++ 3760-assign-elements-to-groups-with-constraints/assign-elements-to-groups-with-constraints.cpp
@@ -1,18 +1,5 @@
 class Solution {
 public:
-    vector<int> factors(int n){
-        vector<int> f;
-        for(int i=1; i*i<=n;i++){
-            if(n%i==0){
-                if(i*i!=n){
-                    f.push_back(i);
-                }
-                f.push_back(n/i);
-            }
-        }
-       
-        return f;
-    }
     vector<int> assignElements(vector<int>& groups, vector<int>& elements) {
         int n = groups.size();
         unordered_map<int,int>m;
@@ -24,13 +11,17 @@ public:
         }
         vector<int>ans(n,1e5+1);
         for(int i=0;i<n;i++){
-            // int x = groups
-            vector<int>f = factors(groups[i]);
-            for(int a:f){
-                if(m[a]){
-                    ans[i]=min(ans[i],m[a]-1);
-                }
+            int x = groups[i];
+            for(int j=1; j*j<=x;j++){
+            if(x%j==0){
+              if(m[j]){
+                ans[i]=min(ans[i],m[j]-1);
+              }
+              if(m[x/j]){
+                ans[i]=min(ans[i],m[x/j]-1);
+              }
             }
+        }
             if(ans[i]==1e5+1)
             ans[i]=-1;
         }
