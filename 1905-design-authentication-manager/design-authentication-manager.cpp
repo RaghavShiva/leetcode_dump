406af9876629d18a8b09827e983145821471e5bf
SHIVA RAGHAV <139120737+RaghavShiva@users.noreply.github.com>
2025-04-20 22:00:25 +0530

    Time: 9 ms (87.17%) | Memory: 38.8 MB (23.23%) - LeetSync

--- 1905-design-authentication-manager/design-authentication-manager.cpp
+++ 1905-design-authentication-manager/design-authentication-manager.cpp
@@ -1,40 +1,91 @@
-class AuthenticationManager 
-{
-    int t;
-    int uniques{};
-    unordered_map<string, int> st;
-    queue<pair<string, int>> q;
-    
+#include <iostream>
+#include <unordered_map>
+#include <string>
+
+using namespace std;
+
+struct Node {
+    string key;
+    int exp;
+    Node* prev;
+    Node* next;
+
+    Node(string k, int e) : key(k), exp(e), prev(nullptr), next(nullptr) {}
+};
+
+class AuthenticationManager {
+    int ttl;
+    unordered_map<string, Node*> lookup;
+    Node* head;
+    Node* tail;
+
 public:
-    AuthenticationManager(int t) : t{t} 
-    {   
+    AuthenticationManager(int timeToLive) {
+        ttl = timeToLive;
+
+        // Sentinel head and tail
+        head = new Node("inf", INT_MAX);
+        tail = new Node("inf", INT_MAX);
+
+        head->next = tail;
+        tail->prev = head;
     }
-    
-    void generate(string s, int ct) 
-    {
-        st[s] = ct+t-1;
-        ++uniques;
-        q.push({s, ct+t-1});
+
+    void generate(string tokenId, int currentTime) {
+        cleanup(currentTime);
+
+        Node* node = new Node(tokenId, currentTime + ttl);
+        lookup[tokenId] = node;
+        addNode(node);
     }
-    
-    void drop(int ct)
-    {
-        for( ; !empty(q) and q.front().second<ct; q.pop())
-            if(st[q.front().first]==q.front().second and  st[q.front().first]<ct) --uniques; 
-    }
-    
-    void renew(string s, int ct) 
-    {
-        if(st[s]>=ct)
-        {
-            st[s] = ct+t-1;
-            q.push({s, ct+t-1});
+
+    void renew(string tokenId, int currentTime) {
+        cleanup(currentTime);
+
+        if (lookup.count(tokenId) && lookup[tokenId]->exp > currentTime) {
+            deleteNode(lookup[tokenId]);
+            lookup.erase(tokenId);
+
+            Node* node = new Node(tokenId, currentTime + ttl);
+            lookup[tokenId] = node;
+            addNode(node);
         }
     }
-    
-    int countUnexpiredTokens(int ct) 
-    {
-        drop(ct);
-        return uniques;     
+
+    int countUnexpiredTokens(int currentTime) {
+        cleanup(currentTime);
+        return lookup.size();
+    }
+
+private:
+    void cleanup(int currentTime) {
+        Node* curr = head->next;
+
+        while (curr != tail && curr->exp <= currentTime) {
+            Node* toRemove = curr;
+            curr = curr->next;
+            lookup.erase(toRemove->key);
+            deleteNode(toRemove);
+        }
+    }
+
+    void addNode(Node* node) {
+        Node* prev = tail->prev;
+
+        prev->next = node;
+        node->prev = prev;
+
+        node->next = tail;
+        tail->prev = node;
+    }
+
+    void deleteNode(Node* node) {
+        Node* prev = node->prev;
+        Node* next = node->next;
+
+        if (prev) prev->next = next;
+        if (next) next->prev = prev;
+
+        delete node;
     }
 };
