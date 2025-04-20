7ac278a4bab5258fd1218b40cf66fa34d79e2d01
SHIVA RAGHAV <139120737+RaghavShiva@users.noreply.github.com>
2025-04-20 21:50:43 +0530

    Time: 3 ms (99.78%) | Memory: 38.5 MB (28.54%) - LeetSync

---
+++ 1905-design-authentication-manager/design-authentication-manager.cpp
@@ -0,0 +1,40 @@
+class AuthenticationManager 
+{
+    int t;
+    int uniques{};
+    unordered_map<string, int> st;
+    queue<pair<string, int>> q;
+    
+public:
+    AuthenticationManager(int t) : t{t} 
+    {   
+    }
+    
+    void generate(string s, int ct) 
+    {
+        st[s] = ct+t-1;
+        ++uniques;
+        q.push({s, ct+t-1});
+    }
+    
+    void drop(int ct)
+    {
+        for( ; !empty(q) and q.front().second<ct; q.pop())
+            if(st[q.front().first]==q.front().second and  st[q.front().first]<ct) --uniques; 
+    }
+    
+    void renew(string s, int ct) 
+    {
+        if(st[s]>=ct)
+        {
+            st[s] = ct+t-1;
+            q.push({s, ct+t-1});
+        }
+    }
+    
+    int countUnexpiredTokens(int ct) 
+    {
+        drop(ct);
+        return uniques;     
+    }
+};
