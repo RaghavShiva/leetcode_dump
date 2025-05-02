8a0b7bcae9bf42dcdf714e7bc3ffdc33a268a4e4
SHIVA RAGHAV <139120737+RaghavShiva@users.noreply.github.com>
2025-05-02 14:38:59 +0530

    Time: 19 ms (50.00%) | Memory: 23 MB (33.93%) - LeetSync

---
+++ 868-push-dominoes/push-dominoes.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    string pushDominoes(string S) {
+        int N = S.size();
+        vector<int> forces(N, 0);
+
+        // Left to Right pass (forces from 'R')
+        int force = 0;
+        for (int i = 0; i < N; ++i) {
+            if (S[i] == 'R') force = N;
+            else if (S[i] == 'L') force = 0;
+            else force = max(force - 1, 0);
+            forces[i] += force;
+        }
+
+        // Right to Left pass (forces from 'L')
+        force = 0;
+        for (int i = N - 1; i >= 0; --i) {
+            if (S[i] == 'L') force = N;
+            else if (S[i] == 'R') force = 0;
+            else force = max(force - 1, 0);
+            forces[i] -= force;
+        }
+
+        // Construct the result based on net forces
+        string result;
+        for (int f : forces) {
+            if (f > 0) result += 'R';
+            else if (f < 0) result += 'L';
+            else result += '.';
+        }
+
+        return result;
+    }
+};
