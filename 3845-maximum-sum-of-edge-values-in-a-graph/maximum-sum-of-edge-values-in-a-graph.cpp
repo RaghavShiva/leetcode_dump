d69eadb7549febbdf840a225de08f7ddad38caed
SHIVA RAGHAV <139120737+RaghavShiva@users.noreply.github.com>
2025-06-08 15:11:35 +0530

    Time: 7 ms (87.53%) | Memory: 250.4 MB (59.47%) - LeetSync

--- 3845-maximum-sum-of-edge-values-in-a-graph/maximum-sum-of-edge-values-in-a-graph.cpp
+++ 3845-maximum-sum-of-edge-values-in-a-graph/maximum-sum-of-edge-values-in-a-graph.cpp
@@ -1,12 +1,15 @@
 class Solution {
 public:
-    long long maxScore(int n, std::vector<std::vector<int>>& edges) {
-        bool isCycle = (n == edges.size());
-        
-        long long nMinus1 = static_cast<long long>(n - 1);
-        long long nPlus6 = static_cast<long long>(n + 6);
-        long long nSquare = static_cast<long long>(n) * n;
-
-        long long result = (nMinus1 * (nMinus1 * nPlus6 + nSquare)) / 6 + 2 * (isCycle ? 1 : 0);
-        return result; }
+    long long maxScore(int n, vector<vector<int>>& edges) {
+        long long ans = 0;
+        ans += (1LL)*n*(n-1);
+        ans += (1LL)*n*(n-2);
+        for(int i=n-3;i>=1;i--){
+            ans += (1LL)*i*(i+2);
+        }
+        if(edges.size()==n-1){
+            return ans;
+        }
+        return ans+2;
+    }
 };
