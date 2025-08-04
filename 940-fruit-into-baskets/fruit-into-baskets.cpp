f1fa481352c455bfe663abc6269d6cebf5fb10bd
SHIVA RAGHAV <139120737+RaghavShiva@users.noreply.github.com>
2025-08-04 23:18:09 +0530

    Time: 1182 ms (5.25%) | Memory: 69.8 MB (98.21%) - LeetSync

---
+++ 940-fruit-into-baskets/fruit-into-baskets.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits) {
+        int n = fruits.size();
+        int ans = 0;
+        int l=-1,r=-1,k=-1;
+        for(int i=0;i<n;i++){
+            if(l==-1){
+                l=i;
+            }
+            else if(fruits[i]!=fruits[l] && r==-1){
+                r=i;
+            }
+            else{
+                if(fruits[i]==fruits[l]){
+                    l=i;
+                }
+                else if(fruits[i]==fruits[r]){
+                    r=i;
+                }
+                else{
+                    if(l<r){
+                        k = l;
+                        l=r;
+                        r=i;
+                    }
+                    else{
+                        k=r;
+                        
+                        r=i;
+                    }
+                }
+            }
+            ans = max(ans,max(l,r)-k);
+            cout<<ans<<' ';
+        }
+        return ans;
+    }
+};
