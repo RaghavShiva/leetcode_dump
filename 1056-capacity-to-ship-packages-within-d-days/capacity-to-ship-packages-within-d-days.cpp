227c6cb8b237d4f2eccd62bd2de803cf3c7eaf4d
SHIVA RAGHAV <139120737+RaghavShiva@users.noreply.github.com>
2025-08-06 11:04:46 +0530

    Time: 19 ms (12.47%) | Memory: 34.9 MB (95.06%) - LeetSync

--- 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
@@ -4,9 +4,6 @@ public:
         int c=0;
         for(int i=0;i<n;i++){
             c+=w[i];
-            if(w[i]>m){
-                return false;
-            }
             if(c>m){
                 d--;
                 c=w[i];
@@ -17,7 +14,7 @@ public:
     int shipWithinDays(vector<int>& weights, int days) {
         int n = weights.size();
         int l, h=1e8;
-        l = *min_element(weights.begin(),weights.end());
+        l = *max_element(weights.begin(),weights.end());
         while(l<=h){
             int m = (l+h)/2;
             if(helper(m,weights,days,n)){
