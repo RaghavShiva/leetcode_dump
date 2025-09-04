55f4cb9324b829eb595a67c4422106df9dbd8f8e
SHIVA RAGHAV <139120737+RaghavShiva@users.noreply.github.com>
2025-09-04 17:12:58 +0530

    Time: 147 ms (39.83%) | Memory: 110.3 MB (47.70%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.cpp
@@ -1,32 +1,41 @@
 class Solution {
 public:
-    bool canPartition(vector<int>& arr) {
-        int n=arr.size();
-        	int s=0;
-	for(int i=0;i<n;i++){
-       s+=arr[i];
-	}
-	if(n==1 || s%2==1)
-	return false;
-	int k=s/2;
-	 vector<bool>prev(k+1);
-     prev[0]=true;
-     if(arr[0]<=k)
-     prev[arr[0]]=true;
-     for(int i=1;i<n;i++){
-         vector<bool>cur(k+1);
-       for(int j=1;j<=k;j++){
-           bool p=false;
-           bool np=false;
-            if(j>=arr[i])
-    p=prev[j-arr[i]];
-    np=prev[j];
-    cur[j]=(p|np);
-       }
-       cur[0]=true;
-      prev=cur;
-     }
-    
-return prev[k];
+    bool helper(vector<int>& nums, int i, int t,  vector<vector<int>> &dp){
+        // base cases
+        if(t==0){
+            return true;
+        }
+        if(t<0){
+            return false;
+        }
+        if(i<0){
+            return false;
+        }
+        if(dp[i][t]!=-1){
+            return dp[i][t];
+        }
+    // main logic
+        bool taken = helper(nums,i-1,t-nums[i],dp);
+        bool not_taken = helper(nums,i-1,t,dp);
+        // updating state
+        dp[i][t] = taken|not_taken;
+        return dp[i][t];
+    }
+    bool canPartition(vector<int>& nums) {
+        int n = nums.size();
+        int sum=0;
+        for(int i=0;i<n;i++){
+            sum+=nums[i];
+        }
+        // sum is odd
+        if(sum%2!=0){
+            return false;
+        }
+
+        int target = sum/2;
+        vector<vector<int>> dp(n,vector<int> (target+1,-1));
+        // space-200*(100*200)
+        // n*sum;
+        return helper(nums,n-1,target,dp);
     }
 };
