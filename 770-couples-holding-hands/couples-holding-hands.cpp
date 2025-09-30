d0c6022514201f8288a7243ba0229aab2e214524
SHIVA RAGHAV <139120737+RaghavShiva@users.noreply.github.com>
2025-09-30 14:55:35 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (80.04%) - LeetSync

--- 770-couples-holding-hands/couples-holding-hands.cpp
+++ 770-couples-holding-hands/couples-holding-hands.cpp
@@ -1,67 +1,25 @@
-class DSU{
-    public:
-    vector<int>size,parent;
-    DSU(int n)
-    {
-        size.resize(n+1,1);
-        parent.resize(n+1);
-        for(int i =0;i<n+1;i++)parent[i]=i;
-    }
-    int findultiparent(int u)
-    {
-        if(parent[u] == u)return u;
-        return parent[u] = findultiparent(parent[u]);
-    }
-    void unionBysize(int u,int v)
-    {
-        int ulp_u = findultiparent(u);
-        int ulp_v = findultiparent(v);
-
-        if(ulp_u == ulp_v) return;
-
-        if(size[ulp_u]>size[ulp_v])
-        {
-            size[ulp_u]+=size[ulp_v];
-            parent[ulp_v] = ulp_u;
-        }
-        else
-        {
-            size[ulp_v]+=size[ulp_u];
-            parent[ulp_u] = ulp_v;
-        }
-    }
-};
 class Solution {
 public:
     int minSwapsCouples(vector<int>& row) {
         int n = row.size();
-        unordered_map<int,int>mpp;
-        for(int i =0;i<n;i++)mpp[row[i]] = i/2;
+        map<int,int> mp;
+        for(int i = 0; i < n; i++) 
+            mp[row[i]] = i;
 
-        DSU ds(n/2);
-        int swaps = 0;
-        for(int i =0;i<n;i++)
-        {
-            if(row[i]%2==0)
-            {
-                int u = row[i],v = row[i]+1;
-                if(ds.findultiparent(mpp[u])!=ds.findultiparent(mpp[v]))
-                {
-                    swaps++;
-                    ds.unionBysize(mpp[u],mpp[v]);
-                }
-            }
-            else
-            {
-                int u = row[i],v = row[i]-1;
-                if(ds.findultiparent(mpp[u])!=ds.findultiparent(mpp[v]))
-                {
-                    swaps++;
-                    ds.unionBysize(mpp[u],mpp[v]);
-                }
+        int ans = 0;
+
+        for(int i = 0; i < n - 1; i += 2){
+            int p = (row[i]%2==0 ? row[i]+1 : row[i]-1);
+
+            if(row[i+1] != p){ 
+
+                int j = mp[p]; 
+                swap(row[i+1], row[j]);
+
+                mp[row[j]]=j; 
+                ans++;
             }
         }
-        return swaps;
-
+        return ans;
     }
 };
