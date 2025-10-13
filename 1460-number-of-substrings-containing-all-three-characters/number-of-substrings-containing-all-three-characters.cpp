d5094571af32543c3b24c461cafbcc447c074f1f
SHIVA RAGHAV <139120737+RaghavShiva@users.noreply.github.com>
2025-10-13 23:42:13 +0530

    Time: 11 ms (48.03%) | Memory: 11.1 MB (34.96%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
@@ -1,39 +1,16 @@
 class Solution {
 public:
     int numberOfSubstrings(string s) {
-        vector<vector<int>> v(3);
-        int n = s.size();
-        for(int i=0;i<n;i++){
-            v[s[i]-'a'].push_back(i);
+        int len = s.length();
+        vector<int> lastPos = {-1, -1, -1};
+        int total = 0;
+
+        for (int pos = 0; pos < len; pos++) {
+            lastPos[s[pos] - 'a'] = pos;
+
+            total += 1 + min({lastPos[0], lastPos[1], lastPos[2]});
         }
-        if(v[0].size()==0 || v[1].size()==0 || v[2].size()==0){
-            return 0;
-        }
-        int a=0,b=0,c=0;
-        int t = max(v[0][a],max(v[1][b],v[2][c]));
-        int ans = n-t;
-        for(int i=1;i<n;i++){
-            if(s[i-1]=='a'){
-                a++;
-                if(a==v[0].size()){
-                    return ans;
-                }
-            }
-            else if(s[i-1]=='b'){
-                b++;
-                if(b==v[1].size()){
-                    return ans;
-                }
-            }
-            else{
-                c++;
-                if(c==v[2].size()){
-                    return ans;
-                }
-            }
-            t = max(v[0][a],max(v[1][b],v[2][c]));
-            ans+=n-t;
-        }
-        return ans;
+
+        return total;
     }
 };
