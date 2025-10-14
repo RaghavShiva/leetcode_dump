5a311f9498628bcb11a47c6c6a1957b3bc082a65
SHIVA RAGHAV <139120737+RaghavShiva@users.noreply.github.com>
2025-10-14 23:31:32 +0530

    Time: 0 ms (100.00%) | Memory: 27.4 MB (97.46%) - LeetSync

--- 221-maximal-square/maximal-square.cpp
+++ 221-maximal-square/maximal-square.cpp
@@ -4,19 +4,19 @@ public:
         if (matrix.empty()) {
             return 0;
         }
-        int m = matrix.size(), n = matrix[0].size(), sz = 0;
-        vector<int> pre(n, 0), cur(n, 0);
+        int m = matrix.size(), n = matrix[0].size(), sz = 0, pre;
+        vector<int> cur(n, 0);
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
+                int temp = cur[j];
                 if (!i || !j || matrix[i][j] == '0') {
                     cur[j] = matrix[i][j] - '0';
                 } else {
-                    cur[j] = min(pre[j - 1], min(pre[j], cur[j - 1])) + 1;
+                    cur[j] = min(pre, min(cur[j], cur[j - 1])) + 1;
                 }
                 sz = max(cur[j], sz);
+                pre = temp;
             }
-            fill(pre.begin(), pre.end(), 0);
-            swap(pre, cur);
         }
         return sz * sz;
     }
