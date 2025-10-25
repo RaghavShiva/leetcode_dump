03345236171bdd54fe2c51e3aa176d15d2a73dd9
SHIVA RAGHAV <139120737+RaghavShiva@users.noreply.github.com>
2025-10-25 14:32:35 +0530

    Time: 7 ms (79.23%) | Memory: 23 MB (15.14%) - LeetSync

--- 477-total-hamming-distance/total-hamming-distance.cpp
+++ 477-total-hamming-distance/total-hamming-distance.cpp
@@ -1,15 +1,25 @@
 class Solution {
 public:
-    int totalHammingDistance(vector<int>& nums) {
-        int ans=0;
-        int n=nums.size();
-        int t=0;
+    int totalHammingDistance(vector<int>& arr) {
+        int sum=0,n=arr.size();
+
+        vector<int>cnt(32);
+        sort(arr.begin(),arr.end());
+
         for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                t=nums[i]^nums[j];
-                ans+=__builtin_popcount(t);
+            int num=arr[i];
+
+            int pos=0;
+            while(num > 0){
+                if(i != 0){
+                    if(num & 1) sum += (i-cnt[pos]);
+                    else sum += cnt[pos];
+                }
+                if(num & 1) cnt[pos]++;
+                num >>= 1;
+                pos++;
             }
         }
-        return ans;
+        return sum;
     }
 };
