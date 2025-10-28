8d12782b6f0649d4070c550b2bd63d9c6e02f27b
SHIVA RAGHAV <139120737+RaghavShiva@users.noreply.github.com>
2025-10-28 15:03:05 +0530

    Time: 4 ms (68.35%) | Memory: 22.6 MB (53.67%) - LeetSync

--- 3616-make-array-elements-equal-to-zero/make-array-elements-equal-to-zero.cpp
+++ 3616-make-array-elements-equal-to-zero/make-array-elements-equal-to-zero.cpp
@@ -1,31 +1,18 @@
 class Solution {
 public:
-    bool solve(int i, int d, vector<int> nums){
-        int n = nums.size();
-        while(i>=0 && i<n){
-            if(nums[i]==0){
-                i+=d;
-            }
-            else{
-                nums[i]--;
-                d*=(-1);
-                i+=d;
-            }
-        }
-        for(int j=0;j<n;j++){
-            if(nums[j]!=0) return false;
-        }
-        return true;
-    }
     int countValidSelections(vector<int>& nums) {
         int n = nums.size();
         int ans = 0;
+        vector<int> v(n+1,0);
+        for(int i=0;i<n;i++){
+            v[i+1] = v[i]+nums[i];
+        }
         for(int i=0;i<n;i++){
             if(nums[i]==0){
-                if(solve(i,-1,nums)){
+                if((v[n]-v[i])-v[i]==1 || (v[n]-v[i])-v[i]==0){
                     ans++;
                 }
-                if(solve(i,1,nums)){
+                if(v[i]-(v[n]-v[i])==1 || v[i]-(v[n]-v[i])==0){
                     ans++;
                 }
             }
