3d04408e12023c267b782e5fad5c9331916a2afe
SHIVA RAGHAV <139120737+RaghavShiva@users.noreply.github.com>
2025-10-28 15:05:28 +0530

    Time: 0 ms (100.00%) | Memory: 22 MB (67.89%) - LeetSync

--- 3616-make-array-elements-equal-to-zero/make-array-elements-equal-to-zero.cpp
+++ 3616-make-array-elements-equal-to-zero/make-array-elements-equal-to-zero.cpp
@@ -3,19 +3,21 @@ public:
     int countValidSelections(vector<int>& nums) {
         int n = nums.size();
         int ans = 0;
-        vector<int> v(n+1,0);
+        int s=0,p=0;
         for(int i=0;i<n;i++){
-            v[i+1] = v[i]+nums[i];
+            s+=nums[i];
         }
         for(int i=0;i<n;i++){
             if(nums[i]==0){
-                if((v[n]-v[i])-v[i]==1 || (v[n]-v[i])-v[i]==0){
+                if(s-p==1 || s-p==0){
                     ans++;
                 }
-                if(v[i]-(v[n]-v[i])==1 || v[i]-(v[n]-v[i])==0){
+                if(p-s==1 || p-s==0){
                     ans++;
                 }
             }
+            s-=nums[i];
+            p+=nums[i];
         }
         return ans;
     }
